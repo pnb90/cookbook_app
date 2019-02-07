5a9571055c66360a29ce0576cf1b6f884cd674b9
pnb90 <phuocbui90@gmail.com>
2019-02-07 12:45:18 -0600

    updated something

--- app/controllers/api/recipes_controller.rb
+++ app/controllers/api/recipes_controller.rb
@@ -20,7 +20,8 @@ class Api::RecipesController < ApplicationController
                           chef: params[:chef],
                           ingredients: params[:ingredients],
                           directions: params[:directions],
-                          prep_time: params[:prep_time]
+                          prep_time: params[:prep_time],
+                          image_url: params[:image_url]
                         )
     @recipe.save
     render 'show.json.jbuilder' #since we already have the ability to just see one
